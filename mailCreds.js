9953f1ce71122e5409fa95ccb3e6c131eedb1799
Harshit <hcool2489@gmail.com>
2020-11-05 06:59:37 +0530

    Delete mailCreds.js

--- mailCreds.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    host: "smtp.gmail.com", // Your mail provider
-    port: 465,
-    secure: true,
-    auth: {
-        user: "email@example.com",
-        pass: "passwordOfEMail"
-    }
-}
